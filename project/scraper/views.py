fb48a4eb4c484863aea90c5048a241beffee0895
akhisyabab <akhisyabab@gmail.com>
2020-03-01 07:01:19 +0700

    updata data to google sheet

--- project/scraper/views.py
+++ project/scraper/views.py
@@ -1,13 +1,19 @@
-from datetime import datetime
-import io
+import gspread
 import csv
+import io
 import requests
+from datetime import datetime
+
 from flask import Blueprint, redirect, url_for, render_template, make_response, Response
 from sqlalchemy.sql import text
 
 from project import db
 from project.models.models import Records, TimeFetched
 from project.utils.date import str2date, date2str
+
+
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 scraper_blueprint = Blueprint('scraper', __name__, template_folder='templates')
 
@@ -58,13 +64,55 @@ def fetch():
 def csv_download():
     output = io.StringIO()
     writer = csv.writer(output)
+    # writer2 = csv.writer(open("usd-rates.csv", 'w')) # create csv file
+
     headers = ['code', 'name', 'date', 'rate']
     writer.writerow(headers)
+    # writer2.writerow(headers)
 
     records = Records.query.all()
     for record in records:
         row = [record.code, record.name, record.date, record.rate]
         writer.writerow(row)
+        # writer2.writerow(row)
 
     output.seek(0)
     return Response(output, mimetype="text/csv", headers={"Content-Disposition": "attachment; filename=usd-rates.csv"})
+
+
+@scraper_blueprint.route('/spreadsheet', methods=['GET', 'POST'])
+def add_to_sheet():
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('drive-public.json', scope)
+    client = gspread.authorize(creds)
+
+    # select spreadsheet
+    sh = client.open('public-sheet')
+
+    # # Delete worksheet
+    # sh.del_worksheet(sh.worksheet("usd-rates"))
+    # # Add worksheet
+    # sh.add_worksheet(title="usd-rates", rows="100", cols="20")
+
+    # # Select worksheet
+    # worksheet = sh.worksheet("usd-rates")
+
+    headers = ['Code', 'Name', 'Date', 'Rate']
+    records = Records.query.all()
+    datas = []
+    datas.append(headers)
+    for data in records:
+        datas.append([data.code, data.name, data.date, data.rate])
+
+    sh.values_update(
+        '{}!A1'.format('usd-rates'),
+        params={'valueInputOption': 'RAW'},
+        body={'values': datas}
+    )
+
+    return redirect(url_for('scraper.home'))
+
+
+
